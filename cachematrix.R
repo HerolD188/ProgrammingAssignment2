81a3e8249af310a1ea78c25478cd375b4b308c68
HerolD188 <davidherold@ieee.org>
2015-05-17 16:47:24 -0400

    First pass at function definition.

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    minv <- NULL
+    set <- function(y) {
+        x <<- y
+        minv <- NULL
+    }
+    get <- function() x
+    setinv <- function(minv1) minv <<- minv1
+    getinv <- function() minv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 
 }
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    minv <- x$getinv()
+    if(!is.null(minv)) {
+        message("getting cached data")
+        return(minv)
+    }
+    data <- x$get()
+    minv <- solve(data, ...)
+    x$setinv(minv)
+    minv
 }
